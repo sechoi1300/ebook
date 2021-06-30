5286e16e5d016e8eed8add90fa73d316a8d17653
FTN2021 <86498766+FTN2021@users.noreply.github.com>
2021-06-29 19:29:41 -0700

    Add files via upload

---
+++ Frontend/install_dependencies.sh
@@ -0,0 +1,2 @@
+npm install @react-navigation/native
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-drawer-layout react-native-safe-area-context @react-native-community/masked-view react-native-elements
